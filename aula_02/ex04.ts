2cd36b67cdd541d40c913d6d9694c14dc1fee5be
EduardoVBF <eduardo.freitas15@fatec.sp.gov.br>
2023-08-21 19:19:47 -0300

    Exercícios aula 18/08,

---
+++ aula_02/ex04.ts
@@ -0,0 +1,11 @@
+//Faça um programa que recebe um número inteiro e verifique se esse número é par ou ímpar.
+
+namespace ex04 {
+    let num: number = 70
+
+    if (num % 2 == 0) {
+        console.log (`O número ${num} é par!`)
+    } else {
+        console.log (`O número ${num} é ímpar!`)
+    }
+}
