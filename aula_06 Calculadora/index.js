73e9485ed59e12c649b296faef006ab492c71cbf
EduardoVBF <eduardo.freitas15@fatec.sp.gov.br>
2023-09-18 20:50:27 -0300

    Calculadora

---
+++ aula_06 Calculadora/index.js
@@ -0,0 +1,49 @@
+const {createApp} = Vue;
+createApp({
+    data() {
+        return {
+            display: "0",
+            numeroAtual: null,
+            numeroAnterior: null,
+            operador: null
+        }
+    },
+    methods: {
+        lidarBotao(botao) {
+            //console.log(botao)
+            switch(botao) {
+                case "+":
+                case "-":
+                case "*":
+                case "/":
+                    this.lidarOperador(botao);
+                    break;
+                case "=":
+                    this.lidarIgual(botao);
+                    break;
+                case ".":
+                    this.lidarDecimal(botao);
+                    break;
+                default:
+                    this.lidarNumeros(botao);
+                    break;
+            }
+        },
+        lidarNumeros(numero) {
+            if(this.display === "0") {
+                this.display = numero;
+            } else {
+                this.display += numero.toString();
+            }
+        },
+        lidarOperador(operador) {
+    
+        },
+        lidarIgual {
+
+        },
+        lidarDecimal {
+
+        }
+    }
+}).mount("#app")
